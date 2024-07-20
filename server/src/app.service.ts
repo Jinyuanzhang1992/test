233ca9b99331b2557a62ef05846e6399b661e1ba
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-07-21 04:15:36 +0930

    squash commits 400

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+   async findAll():Promise<string[]>{
+    return  ['Hello','World'];
+  }
+
+  async findOne():Promise<string> {
+    return 'Hello World';
+  }
 }
